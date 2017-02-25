a5d2004c7a74874848ea3e706cf21647255ffd67
Michał Drzewiecki <michal.drzewiecki@sap.com>
2017-02-25 14:38:02 +0100

    initial commit

---
+++ src/test/java/me/disper/JavaPlaygroundApplicationTests.java
@@ -0,0 +1,16 @@
+package me.disper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaPlaygroundApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
